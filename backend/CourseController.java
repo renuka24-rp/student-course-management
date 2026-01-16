d866c67f9f557fbb93e1d33771ce5130a1284253
Renuka patil <renukapatil.1707@gmail.com>
2026-01-17 03:06:20 +0530

    Update CourseController.java

--- backend/CourseController.java
+++ backend/CourseController.java
@@ -1,1 +1,25 @@
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/courses")
+@CrossOrigin("*")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping
+    public Course addCourse(@RequestBody Course course) {
+        return courseService.addCourse(course);
+    }
+
+    @GetMapping
+    public List<Course> getCourses() {
+        return courseService.getAllCourses();
+    }
+}
 
