cc4395f9de8fb7b3dc4d0b9c5fbf58cbdc49c4b7
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 22:50:25 +0530

    Update CourseService.java

--- backend/CourseService.java
+++ backend/CourseService.java
@@ -1,1 +1,22 @@
+package com.renuka.studentcourse;
 
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public Course saveCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+}
