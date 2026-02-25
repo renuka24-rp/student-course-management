eee5970f0d142366ae2a0b45b6ce730c71ac6172
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 22:55:10 +0530

    Create StudentCourseApplication.java

---
+++ backend/StudentCourseApplication.java
@@ -0,0 +1,12 @@
+package com.renuka.studentcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCourseApplication.class, args);
+    }
+}
