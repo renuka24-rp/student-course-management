72da9c5c65331f5305f2a7d107437a299b1f8549
Renuka patil <renukapatil.1707@gmail.com>
2026-02-25 22:51:50 +0530

    Update EnrollmentController.java

--- backend/EnrollmentController.java
+++ backend/EnrollmentController.java
@@ -1,1 +1,26 @@
+package com.renuka.studentcourse;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/enrollments")
+@CrossOrigin
+public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+    @PostMapping
+    public Enrollment enroll(@RequestBody Enrollment enrollment) {
+        return enrollmentService.enroll(enrollment);
+    }
+
+    @GetMapping
+    public List<Enrollment> getAll() {
+        return enrollmentService.getAllEnrollments();
+    }
+}
